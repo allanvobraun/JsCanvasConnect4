e2632674697b02f983b8ec441bd9bc8e2bdb4f13
allanbraun <allanvobraun@gmail.com>
2021-04-21 23:33:33 -0300

    Arrow positioning

--- src/Game.ts
+++ src/Game.ts
@@ -1,21 +1,36 @@
-import {Colors, Drawable} from "./types.js";
+import {Colors, Coordinates, Drawable} from "./types.js";
 import {canvas, ctx} from "./canvasContext.js";
 import Board from "./Board.js";
 import ArrowHead from "./ArrowHead.js";
+import Disc from "./Disc.js";
 
 class Game {
     drawables: Drawable[] = [];
     board: Board;
     arrow: ArrowHead;
+    arrowPositions: Coordinates[];
+    arrowPositionHeight: number = 80; // relative to the fist top dist
+    arrowSize: number = 40;
     GAME_WIDTH: number = canvas.width;
     GAME_HEIGHT: number = canvas.height;
 
     constructor() {
         this.board = new Board(this.GAME_WIDTH, this.GAME_HEIGHT, Colors.BOARD);
-        this.arrow = new ArrowHead(this.board.position, 40, Colors.RED);
+        this.arrowPositions = this.getArrowPositions();
+
+        this.arrow = new ArrowHead(this.arrowPositions[0], this.arrowSize, Colors.RED);
 
         this.drawables.push(this.board);
         this.drawables.push(this.arrow);
+    }
+
+    getArrowPositions(): Coordinates[] {
+        return this.board.matrix[0].map((disc: Disc) => {
+            return {
+                x: disc.position.x - this.arrowSize / 2,
+                y: disc.position.y - this.arrowPositionHeight
+            }
+        });
     }
 
     run(): void {
@@ -23,4 +38,5 @@ class Game {
         this.drawables.forEach((drawable: Drawable) => drawable.draw());
     }
 }
+
 export default Game;
